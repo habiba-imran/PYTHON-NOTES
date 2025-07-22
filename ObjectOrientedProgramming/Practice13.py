1fde9b6dbc390d395c7b54de6350a518e2c80a39
habiba-imran <habibaimrannn@gmail.com>
2025-07-22 16:37:38 +0500

    Object Oriented Programming done

---
+++ ObjectOrientedProgramming/Practice13.py
@@ -0,0 +1,18 @@
+# METHOD OVERRIDING 
+class Animal:
+    def make_sound(self):
+        print("Some sound")
+    def eat(self):
+        print("Some food")
+class Dog(Animal):
+    def make_sound(self):
+        print("Bark")
+    # how to still use parent method:
+    def eat(self):
+        super().eat()
+# both parent and child class have same method
+d1 = Dog()
+d1.make_sound() # prints bark
+d1.eat() # prints some food
+
+
