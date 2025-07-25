3d9b3dad89290d15d032ab6ffa7ece25147c2a70
habiba-imran <habibaimrannn@gmail.com>
2025-07-25 19:18:59 +0500

    ShutilModule done

---
+++ ShutilModule/Practice.py
@@ -0,0 +1,10 @@
+import shutil
+# shutil.copy("source", "dest") # dest file is created if doesnot exist
+# shutil.copy2("source", "data") # keeps the metadata
+# shutil.copytree("source", "data") # copies folders
+# shutil.move("source", "dest") # equivalant to rename
+# shutil.rmtree("source") # deletes a folder
+# shutil.rm("source") # deletes a file
+# explore other functions yourself
+
+
