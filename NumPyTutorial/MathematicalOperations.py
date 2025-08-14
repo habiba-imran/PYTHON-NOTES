22406c518712836fc87a42832bf802676dcbe486
habiba-imran <habibaimrannn@gmail.com>
2025-08-14 16:43:16 +0500

    Numpy Practice done

---
+++ NumPyTutorial/MathematicalOperations.py
@@ -0,0 +1,15 @@
+# MATHEMATICL EXPRESSIONS ON NUMPPY ARRAY:
+# allows us to traverse through all elements withput actually looping
+import numpy as np
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(a + b)
+print(a - b)
+print(b - a)
+print(a / b) # for float division
+print(a // b) # for integer division
+print(a * b) # [ 4 10 18]
+print(a % b) # modulus [1 2 3]
+print(a ** 2) # every element ^ 2
+
+
